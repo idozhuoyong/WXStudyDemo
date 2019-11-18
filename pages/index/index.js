d92ce3daf8ab1105de514f994920bf81788e8342
小卓子 <idozhuoyong@qq.com>
2019-11-18 17:54:19 +0800

    视图容器 view、scroll-view 学习

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,26 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+// index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
     
   },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad: function () {
+  },
+
+  // click method
+  /**
+   * 视图容器点击
+   */
+  viewBoxTap: function() {
+    wx.navigateTo({
+      url: "../../pages/viewBox/index/index"
+    })
   }
 })
---
+++ pages/viewBox/index/index.js
@@ -0,0 +1,25 @@
+// pages/viewBox/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  // click method
+  viewTap: function(event) {
+    let pagename = event.currentTarget.dataset.pagename;
+    wx.navigateTo({
+      url: "../" + pagename + "/" + pagename,
+    });
+  }
+})
